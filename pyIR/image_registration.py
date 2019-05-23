a24ab672fdd6a5a52c1a520a828e181c62e37072
Emile Parolin <emile.parolin@gmail.com>
2019-05-23 23:06:47 +0200

    Unimportant update.

--- pyIR/image_registration.py
+++ pyIR/image_registration.py
@@ -16,41 +16,55 @@ from gaussians import fit_gaussians
 import matplotlib.pyplot as plt
 na = np.newaxis
 
-original = "../images/Cas1_lung_biopsy/HLA.png"
+source = "../images/Cas1_lung_biopsy/HLA.png"
 target = "../images/Cas1_lung_biopsy/cd.png"
 
-original = "../images/test/malevich0.png"
-target = "../images/test/malevich1.png"
-
-original = "../images/test/cat0.png"
+source = "../images/test/cat0.png"
 target = "../images/test/cat4.png"
+
+source = "../images/test/malevich0.png"
+target = "../images/test/malevich2.png"
+
+source = "../images/test/pacman0.png"
+target = "../images/test/pacman3.png"
+
+source = "../images/test/rectangle0.png"
+target = "../images/test/rectangle1.png"
 
 # Parameters
 n_s = 50 # Number of sampling points
-threshold = .1 # Pixel detection threshold
+threshold = .2 # Pixel detection threshold
+Ng = 3 # Number of gaussians
 # Image models
-Ximg, Ximg_black, Ximg_s = getImage(original, n_s=n_s, threshold=threshold)
+Ximg, Ximg_black, Ximg_s = getImage(source, n_s=n_s, threshold=threshold)
 Yimg, Yimg_black, Yimg_s = getImage(target, n_s=n_s, threshold=threshold)
 # Position and color of all image pixels
-xij, X = Ximg_black
-yij, Y = Yimg_black
+xij, X = Ximg
+yij, Y = Yimg
+# Position and color of image pixels above threshold
+xij_black, X_black = Ximg_black
+yij_black, Y_black = Yimg_black
+# Position and color of image pixels above threshold sample subset
+xij_s, X_s = Ximg_s
+yij_s, Y_s = Yimg_s
+# Translation of target image
+Xmax = np.max(xij[:,0])
+yij[:,0] += Xmax
+yij_black[:,0] += Xmax
+yij_s[:,0] += Xmax
 
 if False :
     # Position and color of sample image pixels
-    x, X_s = Ximg_s
-    y, Y_s = Yimg_s
+    x = xij_s
+    y = yij_s
     # routines for points
     distance = euclidian_distance
     affine_transfo = euclidian_affine_transfo
     pi_prod = points_pi_prod
 else :
-    # Position and color of black image pixels
-    x_points, X_black = Ximg_black
-    y_points, Y_black = Yimg_black
     # Gaussian fits
-    Ng = 25
-    x = fit_gaussians(x_points, Ng)
-    y = fit_gaussians(y_points, Ng)
+    x = fit_gaussians(xij_black, Ng)
+    y = fit_gaussians(yij_black, Ng)
     # routines for gaussians
     distance = wasserstein_distance
     affine_transfo = gaussian_affine_transfo
@@ -61,10 +75,9 @@ res, pi = get_pi(x, y, distance)
 # Compute affine transformation
 sol, A, b = get_t(x, y, pi, distance, affine_transfo, pi_prod)
 # Apply affine transformation to image pixels
-zij = np.dot(A[na,:,:], xij.T).T[:,:,0] + b[na,:]
-
-
+zij_black = np.dot(A[na,:,:], xij_black.T).T[:,:,0] + b[na,:]
+# Plot
 plt.cla()
-plt.scatter(xij[:,0], xij[:, 1], marker=".")
-plt.scatter(yij[:,0], yij[:, 1], marker="x")
-plt.scatter(zij[:,0], zij[:, 1], marker="+")
+plt.scatter(xij_black[:,0], xij_black[:,1], marker=".")
+plt.scatter(yij_black[:,0], yij_black[:,1], marker="x")
+plt.scatter(zij_black[:,0], zij_black[:,1], marker="+")
